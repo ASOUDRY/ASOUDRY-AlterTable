5f9715afbff24e990d8ae6ef65c1a567f4f774d6
ASOUDRY <soudryalex@gmail.com>
2023-02-22 16:24:42 +0000

    done

--- src/main/java/AlterATable.java
+++ src/main/java/AlterATable.java
@@ -45,7 +45,7 @@ public class AlterATable {
      * In problem1.sql, write a SQL statement that adds a "lastname" column to the site_user table, of type
      * varchar(100).
      *
-     *   site_user table:
+     *   site_user
      *   |    id    |     firstname     |
      *   --------------------------------
      *   |1         |'Kevin'            |
